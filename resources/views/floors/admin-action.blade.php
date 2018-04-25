1925b830ae523ce3e62aff018af3a698bab74219
GhaidaaAhmed <ghaidaa.13015@gmail.com>
2018-04-25 22:03:35 +0200

    modify admin-action view

--- resources/views/floors/admin-action.blade.php
+++ resources/views/floors/admin-action.blade.php
@@ -1,6 +1,8 @@
-
-<a href="/floors/edit/{{$id}}"  type="button" class="btn btn-xs btn-primary" ><i class="fa fa-pencil-square"></i></a>
-<form action="/floors/delete/{{$id}}" onsubmit="return confirm('Do you really want to delete?');" method="post" >
-    {{csrf_field()}}
-{{method_field('Delete')}}
-<input name="_method" value="delete" type="submit" class="btn btn-danger" /></form>
+   
+<form action="{{ URL::to('/floors/delete/'. $id ) }}" onsubmit="return confirm('Do you really want to delete?');" method="post" >
+    <a href="/floors/edit/{{$id}}"  type="button" class="btn btn-xs btn-primary" ><i class="fa fa-pencil-square"></i></a>
+    <button name="_method" value="delete" class="btn btn-danger" >
+        <i class="fa fa-trash go inline" aria-hidden="true"></i>
+    </button>
+    {!! csrf_field() !!}
+    {{method_field('Delete')}}</form>
