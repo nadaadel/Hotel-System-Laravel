315bc8f41b0bfe00c18109569a5dfaead51b75a7
Yasminabdallah <yasminabdalllahgaberm@gmail.com>
2018-04-27 13:32:26 +0200

    edit on jwt

--- app/Http/Controllers/Api/RoomController.php
+++ app/Http/Controllers/Api/RoomController.php
@@ -32,7 +32,7 @@ class RoomController extends Controller
      {
         $credentials = request(['email', 'password']);
 
-        if (! $token = auth()->attempt($credentials)) {
+        if (! $token = auth('api')->attempt($credentials)) {
             return response()->json(['error' => 'Unauthorized'], 401);
         }
 
@@ -62,7 +62,7 @@ class RoomController extends Controller
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
-            'expires_in' => auth()->factory()->getTTL() * 60
+            'expires_in' => auth('api')->factory()->getTTL() * 60
         ]);
     }
 
